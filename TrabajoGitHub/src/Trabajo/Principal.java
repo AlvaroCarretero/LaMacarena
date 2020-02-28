de796ea22c43b47e699969f4590a05dda2698f9c
AlvaroCarretero <alvaro.c.m23@gmail.com>
2020-02-28 12:49:24 +0100

    Sumario Requerido
    Estoy haciendo cambios y aqui no pasa nada. Elena, me has timao

---
+++ TrabajoGitHub/src/Trabajo/Principal.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Trabajo;
+
+
+public class Principal {
+
+    public static void main(String[] args) {
+        System.out.println("Esto significa que funciona");
+        System.out.println("O no");
+        System.out.println("Esto es una movida");
+
+    }
+}
