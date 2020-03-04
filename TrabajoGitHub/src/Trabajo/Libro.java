ebaaef2e521ffdb7d152a962cb5b3081da4f2fdf
DaniRodriNa <59997699+DaniRodriNa@users.noreply.github.com>
2020-03-04 18:41:21 +0100

    Libro de pp

--- TrabajoGitHub/src/Trabajo/Libro.java
+++ TrabajoGitHub/src/Trabajo/Libro.java
@@ -6,8 +6,55 @@
 
 package Trabajo;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 
 public class Libro {
+    boolean tituLi=false;
+    int numPag;
+    String titulo,autor;
+    int cantLibros=0;
+
+    public Libro() {
+        pedirDatos();
+        pedirLibros();
+    }
     
+    void pedirAutor(){
+        autor=pedirDatos.PedirDatosTeclado.pedirString("\tIndique el nombre del autor: ");
+    }
+    void pedirNumPag(){
+        int num = 0;
+        boolean bien = false;
+        Scanner pedir = new Scanner(System.in);
+
+        do {
+            try {
+                num = pedir.nextInt();
+                if (num < 0) {
+                    System.out.println("\t\tLo siento ha insertado un numero negrativo");
+                } else {
+                    bien = true;
+                }
+            } catch (InputMismatchException e) {
+                pedir.nextLine();
+                System.out.println("\t\tLo siento, ha insertado letras.");
+            }
+        } while (bien == false);
+    }
+    void pedirTitulo(){
+        titulo=pedirDatos.PedirDatosTeclado.pedirString("\tInserte el titulo del libro: ");
+    }
+    void pedirDatos(){
+        pedirAutor();
+        pedirNumPag();
+    }
+    void pedirLibros(){
+        while(true){
+        pedirTitulo();
+        cantLibros++;
+        }
+    }
 
 }
